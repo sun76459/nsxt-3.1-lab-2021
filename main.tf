10ea55035a40181e3c51bb96b6adb670dff488e8
Yanjun Hu <sun76459@gmail.com>
2021-03-07 16:38:07 -0500

    delete test3 T1 gateway

--- main.tf
+++ main.tf
@@ -88,8 +88,3 @@ resource "nsxt_policy_tier1_gateway" "test2" {
   nsx_id          = "test2"
   display_name    = "test2"
 }
-
-resource "nsxt_policy_tier1_gateway" "test3" {
-  nsx_id          = "test3"
-  display_name    = "test3"
-}
